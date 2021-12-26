252075dbb98e136575720abec47a859956eaa36a
Alejandro <alejandro.castillo@flat101.es>
2021-12-26 10:36:20 +0100

    first commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app class="layout">
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Alejandro Castillo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Sowtware Developer
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "About", icon: "mdi-account-box-outline" },
+        { title: "Projects", icon: "mdi-apps" },
+        { title: "Education", icon: "mdi-school-outline" },
+        { title: "Contact", icon: "mdi-message-text-outline" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+div.v-application--wrap {
+  flex-direction: row;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <section>
+      <v-container>
+        <v-row>
+          <v-col sm="6">
+            <g-image alt="Example image" src="~/profile.png" width="400" />
+          </v-col>
+          <v-col>
+            <h1>Hi, I'm Alex</h1>
+            <p>I'm a software developer...</p>
+          </v-col sm="6">
+        </v-row>
+      </v-container>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
