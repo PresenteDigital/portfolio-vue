3d26c403bbc023ccc349e8d28c14eec237f8bca3
Kosiro <kosiro.developer@gmail.com>
2021-12-30 11:40:05 +0100

    developing medical assistance

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,36 +1,21 @@
 <template>
   <Layout>
-    <Menu />
     <Icons />
     <MedicalAssistance />
-    <About />
-    <Projects />
-    <Education />
-    <Contact />
   </Layout>
 </template>
 
 <script>
-import Menu from "@/components/Menu.vue";
 import Icons from "@/components/Icons.vue";
 import MedicalAssistance from "@/components/MedicalAssistance.vue";
-import About from "@/components/About.vue";
-import Projects from "@/components/Projects.vue";
-import Education from "@/components/Education.vue";
-import Contact from "@/components/Contact.vue";
 
 export default {
   metaInfo: {
     title: "Hello, world!",
   },
   components: {
-    Menu,
     Icons,
     MedicalAssistance,
-    About,
-    Projects,
-    Education,
-    Contact,
   },
 };
 </script>
